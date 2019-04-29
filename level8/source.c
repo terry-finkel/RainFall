b23d89ce84047aebc8d8d97d9574a039a133cf05
Terry Finkel <nfinkel@student.42.fr>
2019-04-29 16:58:35 +0200

    Source level8 WIP

---
+++ level8/source.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int
+main (void) {
+
+    while (1) {
+
+        printf("%p, %p \n", 0x8049aac, 0x8049ab0);
+
+        char buff[32];
+        if (fgets(buff, 128, stdin) == NULL) break;
+        if (strncmp("auth ", buff, 5) == 0) {}
+        if (strncmp("reset", buff, 5) == 0) free((void *) 0x8049aac);
+        if (strncmp("service", buff, 6) == 0) strdup();
+        if (strncmp("login", buff, 5)) {}
+        if () system("/bin/sh");
+    }
+}
