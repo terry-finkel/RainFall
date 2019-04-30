7325119c4a153f4686e1efc1be3c0ab9a5cc8b47
Terry FINKEL <nfinkel@student.42.fr>
2019-04-30 15:57:59 +0200

    level8 and level9 flags

--- level8/source.c
+++ level8/source.c
@@ -9,10 +9,10 @@ main (void) {
 
         char buff[32];
         if (fgets(buff, 128, stdin) == NULL) break;
-        if (strncmp("auth ", buff, 5) == 0) {}
+        if (strncmp("auth ", buff, 5) == 0) malloc(4);
         if (strncmp("reset", buff, 5) == 0) free((void *) 0x8049aac);
         if (strncmp("service", buff, 6) == 0) strdup();
         if (strncmp("login", buff, 5)) {}
-        if () system("/bin/sh");
+        if (((unsigned int *)0x8049aac)[32]) system("/bin/sh");
     }
 }
